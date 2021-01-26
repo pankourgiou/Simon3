3085432828d014d0f01ab9fa9c72406cb66cca94
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-01-26 06:07:15 +0200

    Create Simon3.scm

---
+++ Simon3.scm
@@ -0,0 +1,10 @@
+(string=? "Paranoid Arkanoid" "Paranoid Arkanoid")      ;; => #t
+(string=? "kid aiea eiao" "kid aiea eiao")  ;; => #t
+(string=? "same moon's ox" "same moon's ox");; => #t
+(string=? "Beckham Bartender" "Beckham Bartender")                    ;; => #t
+(string=? "None of your history books mention me? laughed Daria. They will now" "None of your history books mention me? laughed Daria. They will now")     
+(string=? "To outrace the griffin-Kipamu expression meaning to do the impossible"  "To outrace the griffin-Kipamu expression meaning to do the impossible")
+(string=? "Suse4less"  "Suse4less") 
+(string=? "Ubuntu4less"  "Ubuntu4less") 
+(string=? "Puppy4less"  "Puppy4less")
+(string=? "Kali4less"  "Kali4less")    
